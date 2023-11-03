bffe1631cd12651807bbf22501bcb74d8b57716a
Petteri Peltonen <petteri_peltonen@hotmail.com>
2023-11-03 11:51:00 +0200

    wrote more benchmarks

--- gpu_chemistry/benchmark/others/Benchmark-others.cu
+++ gpu_chemistry/benchmark/others/Benchmark-others.cu
@@ -129,13 +129,96 @@ TEST_CASE("gpuODESystem"){
 
 }
 
+TEST_CASE("gpuReaction"){
+
+    using namespace FoamGpu;
+    auto reactions = toDeviceVector(makeGpuReactions(mech));
+
+    const gLabel nReactions = reactions.size();
+    const gLabel nSpecie = TestData::speciesCount(mech);
+    const gLabel nEqns = TestData::equationCount(mech);
+
+    const gScalar p = 1E5;
+    const gScalar T = 900.0;
+
+    device_vector<gScalar> c = [=](){
+        std::vector<gScalar> vals(nSpecie);
+        assign_test_concentration(vals, mech);
+        return device_vector<gScalar>(vals.begin(), vals.end());
+    }();
+
+    device_vector<gScalar> dndt = [=](){
+        std::vector<gScalar> vals(nEqns);
+        return device_vector<gScalar>(vals.begin(), vals.end());
+    }();
+
+    BENCHMARK("dNdtByV"){
+
+        auto op =
+        [
+            =,
+            c = make_mdspan(c, extents<1>{nSpecie}),
+            dndt = make_mdspan(dndt, extents<1>{nEqns}),
+            reactions = make_mdspan(reactions, extents<1>{nReactions})
+        ]__device__(){
+
+            gScalar ret = 0.0;
+            for (int i = 0; i < nReactions; ++i){
+
+                reactions[i].dNdtByV(p, T, c, dndt);
+                ret += dndt[4];
+            }
+            return ret;
+
+        };
+
+        return eval(op);
+    };
+
+
+    device_vector<gScalar> work1(nEqns);
+    device_vector<gScalar> work2(nEqns);
+    device_vector<gScalar> J(nEqns * nEqns);
+
+    BENCHMARK("ddNdtByVdcTp"){
+
+        auto op =
+        [
+            =,
+            c = make_mdspan(c, extents<1>{nSpecie}),
+            work1 = make_mdspan(work1, extents<1>{nEqns}),
+            work2 = make_mdspan(work2, extents<1>{nEqns}),
+            dJ = make_mdspan(J, extents<2>{nEqns, nEqns}),
+            reactions = make_mdspan(reactions, extents<1>{nReactions})
+        ]__device__(){
+
+            gScalar ret = 0.0;
+            for (int i = 0; i < nReactions; ++i){
+                const auto& reaction = reactions[i];
+                auto params = computeReactionParameters(reaction, c, p, T);
+                reaction.ddNdtByVdcTp( p, T, c, dJ, work1, work2, params);
+
+                ret += dJ(4,4);
+            }
+            return ret;
+
+        };
+
+        return eval(op);
+    };
+
+
+
+
+}
+
 
 TEST_CASE("gpuODESolver"){
 
     using namespace FoamGpu;
 
-    auto gpu_thermos = toDeviceVector(makeGpuThermos(mech));
-    auto gpu_reactions = toDeviceVector(makeGpuReactions(mech));
+    auto thermos = toDeviceVector(makeGpuThermos(mech));
+    auto reactions = toDeviceVector(makeGpuReactions(mech));
 
     const gLabel nCells = 1;
     const gLabel nSpecie = TestData::speciesCount(mech);
@@ -145,53 +228,100 @@ TEST_CASE("gpuODESolver"){
     gpuODESystem system
     (
         nEqns,
-        gLabel(gpu_reactions.size()),
-        make_raw_pointer(gpu_thermos.data()),
-        make_raw_pointer(gpu_reactions.data())
+        gLabel(reactions.size()),
+        make_raw_pointer(thermos.data()),
+        make_raw_pointer(reactions.data())
     );
 
-    std::vector<gScalar> vals(nEqns);
-    assign_test_condition(vals, mech);
-    //fill_random(vals);
-    const device_vector<gScalar> y0(vals.begin(), vals.end());
+
+    const device_vector<gScalar> y0 = [=](){
+        std::vector<gScalar> vals(nEqns);
+        assign_test_condition(vals, mech);
+        return device_vector<gScalar>(vals.begin(), vals.end());
+    }();
+
+    const device_vector<gScalar> dy = [=](){
+        std::vector<gScalar> vals(nEqns);
+        fill_random(vals);
+        return device_vector<gScalar>(vals.begin(), vals.end());
+    }();
+
+
     device_vector<gScalar> y(y0.size());
+
 
     memoryResource_t memory(nCells, nSpecie);
     auto buffers = toDeviceVector(splitToBuffers(memory));
 
-
-    const gScalar xStart = 0.0;
-    const gScalar xEnd = 2E-7;
-    const gScalar dxTry = 1E-7;
+    const gScalar dx = 1E-7;
 
 
+    {
 
-    //i.absTol =
+        gpuRosenbrock12<gpuODESystem> solver(
+            system, TestData::makeGpuODEInputs("Rosenbrock12", mech));
 
-    SECTION("Rosenbrock12"){
-        auto i = TestData::makeGpuODEInputs("Rosenbrock12", mech);
-        auto solver = make_gpuODESolver(system, i);
-
-        auto op = [        =,
-                   buffers = make_mdspan(buffers, extents<1>{nCells}),
-                   y0       = make_mdspan(y0, extents<1>{nEqns}),
-                   y       = make_mdspan(y, extents<1>{nEqns})
-                  ] __device__() {
-            gScalar temp = dxTry;
-            for (int i = 0; i < nEqns; ++i) {
-                y(i) = y0(i);
-            }
-
-            solver.solve(xStart, xEnd, y, temp, buffers[0]);
-            return temp;
-        };
-
+        auto op =
+            [        =,
+             buffers = make_mdspan(buffers, extents<1>{nCells}),
+             y0      = make_mdspan(y0, extents<1>{nEqns}),
+             y       = make_mdspan(y, extents<1>{nEqns}),
+             dy = make_mdspan(dy, extents<1>{nEqns})
+             ] __device__() {
+                solver.solve(y0, dy, dx, y, buffers[0]);
+                return y[4];
+            };
 
         BENCHMARK("Rosenbrock12"){
             return eval(op);
         };
 
     }
+
+    {
+
+        gpuRosenbrock23<gpuODESystem> solver(
+            system, TestData::makeGpuODEInputs("Rosenbrock23", mech));
+
+        auto op =
+            [        =,
+             buffers = make_mdspan(buffers, extents<1>{nCells}),
+             y0      = make_mdspan(y0, extents<1>{nEqns}),
+             y       = make_mdspan(y, extents<1>{nEqns}),
+             dy = make_mdspan(dy, extents<1>{nEqns})
+             ] __device__() {
+                solver.solve(y0, dy, dx, y, buffers[0]);
+                return y[4];
+            };
+
+        BENCHMARK("Rosenbrock23"){
+            return eval(op);
+        };
+
+    }
+
+    {
+
+        gpuRosenbrock34<gpuODESystem> solver(
+            system, TestData::makeGpuODEInputs("Rosenbrock34", mech));
+
+        auto op =
+            [        =,
+             buffers = make_mdspan(buffers, extents<1>{nCells}),
+             y0      = make_mdspan(y0, extents<1>{nEqns}),
+             y       = make_mdspan(y, extents<1>{nEqns}),
+             dy = make_mdspan(dy, extents<1>{nEqns})
+             ] __device__() {
+                solver.solve(y0, dy, dx, y, buffers[0]);
+                return y[4];
+            };
+
+        BENCHMARK("Rosenbrock34"){
+            return eval(op);
+        };
+
+    }
+
 
 
 
